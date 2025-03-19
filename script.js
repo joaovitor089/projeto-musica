4cd2bb3b8288cbc4c67dd2c4c9ef14e15f9552ca
joaovitor089 <joao.chevonica.barbosa@escola.pr.gov.br>
2025-03-19 19:42:33 -0300

    n,bh

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let audios = [
-    {caminho:'https://youtu.be/O9F1XMwJG1k?si=IfeeiblGbHg7LwrR, legenda:'Boi soberano'},
+    {caminho:'kkkkk, legenda:'Boi soberano'},
     {caminho:'https://youtu.be/sscvxKHfW4c?si=lCUEkAPiY1lEfSmM, legenda:'Boate Azul'},
     {caminho:'https://youtu.be/M3w2xDPHr90?si=8ZFooTL2ibkzerXD, legenda:'Mamba negra'},
     {caminho:'https://youtu.be/vOElyIyRPdE?si=ZB391HluNO5jzn56', Portal do universo:'Hayit'},
     {caminho:'https://youtu.be/KeUSDZ6xkYA?si=gSnuytdYNkRX8GKJ', Um bom lugar:'Helena'},
     {caminho:'https://youtu.be/TfJsiSMC0U8?si=q9bhxqs207_Zdr6V', legenda:'Dormi na praça'},
     {caminho:'https://youtu.be/8Pilhg_dIQs?si=mvb78jU69TF4RYUt', legenda:'Céu azul'},
-    {caminho:'https://youtu.be/98BMxJU1AGw?si=yVpTyfBPH9LMlyrj', legenda:'Nz conta da loucura'},
+    {caminho:', legenda:'Nz conta da loucura'},
     {caminho:´https://youtu.be/EtTbS-KdcrE?si=zWxo1Uj3Fqn3AMDo', legenda:'Fundo da grota'},
 ];
 
